5727cb8b6e93c7fb6bd25fa27c0730e58f9fbacd
Vincent <vincent@keewe.eu>
2023-07-24 18:11:26 +0200

    init

---
+++ SearchCompanies/index.ts
@@ -0,0 +1,37 @@
+﻿import * as hubspot from "@hubspot/api-client";
+/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an orchestrator function.
+ *
+ * Before running this sample, please:
+ * - create a Durable orchestration function
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your
+ *   function app in Kudu
+ */
+
+import { AzureFunction, Context } from "@azure/functions";
+import { SimplePublicObjectWithAssociations } from "@hubspot/api-client/lib/codegen/crm/companies";
+
+const activityFunction: AzureFunction = async function (
+  context: Context
+): Promise<SimplePublicObjectWithAssociations[]> {
+  context.log("HTTP trigger function processed a request.");
+  const hubspotClient = new hubspot.Client({
+    accessToken: "pat-eu1-7b7968a5-d9f4-4960-8113-496d0a78624f",
+  });
+
+  try {
+    const apiResponse = await hubspotClient.crm.companies.searchApi.doSearch(
+      context.bindingData.data
+    );
+
+    return apiResponse.results;
+  } catch (e) {
+    e.message === "HTTP request failed"
+      ? console.error(JSON.stringify(e.response, null, 2))
+      : console.error(e);
+  }
+};
+
+export default activityFunction;
---
+++ TriggerHttpOrchestrator/index.ts
@@ -0,0 +1,18 @@
+﻿import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import * as df from "durable-functions";
+
+const httpStart: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<any> {
+  const client = df.getClient(context);
+  const instanceId = await client.startNew(
+    req.params.functionName,
+    undefined,
+    req.body
+  );
+
+  return client.createCheckStatusResponse(context.bindingData.req, instanceId);
+};
+
+export default httpStart;
